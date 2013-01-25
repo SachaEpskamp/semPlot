a0ff3dd615869ddb3e755ae6db0c72ba15862b72
Sacha Epskamp <sacha.epskamp@gmail.com>
2013-01-25 17:14:41 +0100

    Added curve pivots to help example.

---
+++ R/zzz.R
@@ -0,0 +1,8 @@
+# I copied this piece of code from Lavaan mainly:
+
+.onAttach <- function(libname, pkgname) {
+  version <- read.dcf(file=system.file("DESCRIPTION", package=pkgname),
+                      fields="Version")
+  packageStartupMessage("This is ",paste(pkgname, version))
+  packageStartupMessage(pkgname, " is BETA software! Please report any bugs.")
+}
