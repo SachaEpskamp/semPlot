ceda750360be559cc890cf3d14dc03b41a975215
Sacha Epskamp <sacha.epskamp@gmail.com>
2013-03-05 10:40:34 +0100

    modelmatrices update

--- R/modelMatrices.R
+++ R/modelMatrices.R
@@ -1,6 +1,41 @@
 # Function to extract parameters into model matrices:
 # Object is semPlotModel or can be created:
 
+# Inner functions:
+getRAMmodel <- function(object)
+{
+  
+  # Parameters:
+  Nvar <- nrow(object@Vars)
+  Nman <- sum(object@Vars$manifest)
+  Names <- object@Vars$name
+  
+  # Empty matrices:
+  A <- S <- matrix(0,Nvar,Nvar)
+  F <- cbind(diag(1,Nman),matrix(0,Nman,Nvar-Nman))
+  F[,order(object@Vars$manifest,decreasing=TRUE)] <- F
+  rownames(A) <- colnames(A) <- rownames(S) <- colnames(S) <- colnames(F) <- Names
+  rownames(F) <- Names[object@Vars$manifest]  
+  
+  # Fill matrices:
+  for (i in seq_len(nrow(object@Pars)))
+  {
+    if (object@Pars$edge[i]=="<->")
+    {
+      S[which(Names==object@Pars$lhs[i])[1],which(Names==object@Pars$rhs[i])[1]] <- 
+        S[which(Names==object@Pars$rhs[i])[1],which(Names==object@Pars$lhs[i])[1]] <- object@Pars$est[i]
+    }
+    if (object@Pars$edge[i]%in%c("->","~>"))
+    {
+      A[which(Names==object@Pars$rhs[i])[1],which(Names==object@Pars$lhs[i])[1]] <- object@Pars$est[i]
+    }
+  }
+  
+  Res <- list(A=A,S=S,F=F)
+  class(Res) <- "RAM"
+  return(Res)  
+}
+
 modelMatrices <- function(object,model="ram")
 {
   # Check if input is combination of models:
